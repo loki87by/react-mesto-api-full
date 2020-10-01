d21735fba71a00a03ba61121869e9b96c44014d9
Alexey Akulich <loki1987by@yandex.ru>
2020-10-01 17:29:54 +0300

    crash-test

--- app.js
+++ app.js
@@ -25,7 +25,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb-14', {
 // **функционал
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email({ tlds: { allow: false } }),
@@ -56,10 +60,12 @@ app.use((err, req, res, next) => {
   });
   next(err);
 });
+/*
 // eslint-disable-next-line func-names
 process.on('uncaughtException', function (err) {
   console.log(err);
 });
+*/
 app.listen(PORT, () => {
   console.log('Server started');
 });
