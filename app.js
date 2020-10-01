5bae0597f289f6a05b4654ba4dc1a647b0d3d0c3
Alexey Akulich <loki1987by@yandex.ru>
2020-10-01 20:50:45 +0300

    try 4

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb-14', {
 // **функционал
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-/*
+
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
-*/
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email({ tlds: { allow: false } }),
