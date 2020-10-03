a292d15c912ae03f62b3542fcad7c7f2465cac48
Alexey Akulich <loki1987by@yandex.ru>
2020-10-03 14:19:37 +0300

    u0iholih

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 /* eslint-disable prefer-arrow-callback */
 // **импорты
 const express = require('express');
-const userLimit = require('express-rate-limit');
+// const userLimit = require('express-rate-limit');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const cors = require('cors');
+// const cors = require('cors');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const createUserRouter = require('./routes/createUser');
@@ -19,27 +19,27 @@ const { pattern } = require('./routes/pattern');
 // const { login, createUser } = require('./controllers/users');
 const { PORT = 3000 } = process.env;
 const app = express();
-
+/*
 const limiter = userLimit({
   windowMs: 1000,
   max: 5,
 });
-
+*/
 // **подключение к БД
 mongoose.connect('mongodb://localhost:27017/mestodb-14', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
-})
-  .then(() => console.log('DB connected'))
+});
+/* .then(() => console.log('DB connected'))
   .catch((err) => {
     console.log(err);
-  });
+  }); */
 
 // **функционал
-app.use(limiter);
-app.use(cors({ origin: true }));
+// app.use(limiter);
+// app.use(cors({ origin: true }));
 // *парсеры
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
