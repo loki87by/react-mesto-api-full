3ebd56ae5effaa486e87015f9762b62fcefc08e1
Alexey Akulich <loki1987by@yandex.ru>
2020-10-03 13:49:10 +0300

    Revert "ывапывпаыв"
    This reverts commit f87c56e6189bdd2b44ad6185e85340cf7593c924.

--- controllers/users.js
+++ controllers/users.js
@@ -8,7 +8,7 @@ const User = require('../models/user');
 const NotFoundError = require('../errors/notFoundErr');
 /* const BadRequestError = require('../errors/badRequest');
 
-// const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 // **список пользователей
 module.exports.getUsers = (req, res, next) => {
@@ -96,7 +96,7 @@ module.exports.login = (req, res) => {
   return User.findUserByCredentials(email, password)
   // return User.findOne({ email })(+password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch((err) => {
