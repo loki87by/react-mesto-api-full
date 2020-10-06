886f72fa9991afb00cb5e22afdde24af255a8959
Alexey Akulich <loki1987by@yandex.ru>
2020-10-06 16:32:46 +0300

    try40

--- controllers/users.js
+++ controllers/users.js
@@ -37,7 +37,7 @@ module.exports.getMyInfo = (req, res) => {
 
 // **получение пользователя по айдишнику
 module.exports.getCurrentUser = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params._id)
     .orFail(new Error('NotValidId'))
     .then((user) => res.send({ data: user }))
     .catch((err) => {
@@ -74,8 +74,8 @@ module.exports.createUser = (req, res) => {
 // *обновление текстовой инфы
 module.exports.updateUser = (req, res, next) => {
   const { name, about } = req.body;
-  const { id } = req.params;
-  return User.findOneAndUpdate({ id }, { name, about },
+  const { _id } = req.params;
+  return User.findByIdAndUpdate({ _id }, { name, about },
     { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
@@ -90,8 +90,8 @@ module.exports.updateUser = (req, res, next) => {
 // *обновление аватара
 module.exports.updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  const { id } = req.params;
-  return User.findOneAndUpdate({ id }, { avatar },
+  const { _id } = req.params;
+  return User.findByIdAndUpdate({ _id }, { avatar },
     { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
