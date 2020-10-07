b8ecb3983dd69c022439dd29371aa1eba3fbc97b
Alexey Akulich <loki1987by@yandex.ru>
2020-10-07 18:59:44 +0300

    88

--- controllers/users.js
+++ controllers/users.js
@@ -25,8 +25,8 @@ module.exports.getUsers = (req, res) => {
 
 // **получение своих данных
 module.exports.getMyInfo = (req, res, next) => {
-  const { _id } = req.user;
-  User.findOne(_id)
+  const { id } = req.user;
+  User.findOne(id)
     .then((user) => {
       if (!user) throw new NotFoundError('Нет такого пользователя');
       res.send(user);
