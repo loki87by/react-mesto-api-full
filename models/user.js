f83248cbaeb3616a63ed28cc6504c2148db11321
Alexey Akulich <loki1987by@yandex.ru>
2020-10-07 22:43:10 +0300

    222

--- models/user.js
+++ models/user.js
@@ -49,7 +49,7 @@ const userSchema = new mongoose.Schema({
 });
 // eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select(+'password')
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
