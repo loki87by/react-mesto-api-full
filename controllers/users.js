59c7a65174a128b564b9e488be0b6b9edab4ddfb
Alexey Akulich <loki1987by@yandex.ru>
2020-10-10 11:30:18 +0300

    4343

--- controllers/users.js
+++ controllers/users.js
@@ -118,7 +118,7 @@ module.exports.login = (req, res) => {
   // return User.findOne({ email })('+password')
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      res.send(token);
+      res.send({ token });
     })
     .catch((err) => {
       res.status(401).send({ message: err.message });
