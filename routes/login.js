a10cdcb81187974cfcddf63a9beaa6d284b80306
Alexey Akulich <loki1987by@yandex.ru>
2020-10-11 14:32:20 +0300

    35

--- routes/login.js
+++ routes/login.js
@@ -1,18 +1,13 @@
 /* eslint-disable linebreak-style */
 // **импорт
 const loginRouter = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+// const { celebrate, Joi } = require('celebrate');
 const { login } = require('../controllers/users');
 
 // **функционал
 // eslint-disable-next-line arrow-body-style
 
-loginRouter.post('/', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email({ tlds: { allow: false } }),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')).required(),
-  }),
-}), login);
+loginRouter.post('/', login);
 
 // **экспорт
 module.exports = loginRouter;
