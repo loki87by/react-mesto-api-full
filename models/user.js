56366d1a95d106df5b68cd73a3e9473c23bce0b6
Alexey Akulich <loki1987by@yandex.ru>
2020-10-12 14:57:25 +0300

    full buggfix

--- models/user.js
+++ models/user.js
@@ -61,20 +61,6 @@ userSchema.statics.findUserByCredentials = function (email, password) {
       return user;
     }));
 };
-/*
-  .then((user) => {
-      if (!user) {
-        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-      }
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-          }
-          return user;
-        });
-    });
-};
-*/
+
 // **экспорт
 module.exports = mongoose.model('user', userSchema);
